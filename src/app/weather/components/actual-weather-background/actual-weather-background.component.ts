9373298c6848b47d6741005ef790f151312c08a4
jhonnoriega1221 <jhonlex17@gmail.com>
2022-09-04 13:41:34 -0500

    feat: background opacity implement in forecast page

--- src/app/weather/components/actual-weather-background/actual-weather-background.component.ts
+++ src/app/weather/components/actual-weather-background/actual-weather-background.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-actual-weather-background',
@@ -9,9 +9,19 @@ export class ActualWeatherBackgroundComponent implements OnInit {
 
   @Input() backgroundURL:string = '';
 
+  private scroll:any;
+
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.onWindowScroll);
   }
 
+  onWindowScroll(e:any) {
+    const scrollHeigth = 391;
+    let actualScroll = window.scrollY;
+    let actualOpacity = (100*actualScroll)/scrollHeigth;
+      document.getElementById('background-opacity')?.style.setProperty('opacity',actualOpacity+'%');
+    
+  }
 }
