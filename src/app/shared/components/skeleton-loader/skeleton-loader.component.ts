18a9caca37f3bd556fd10f7c8da31a2cf52d0786
jhonnoriega1221 <jhonlex17@gmail.com>
2022-12-30 16:03:44 -0500

    feat: dialogs and skeleton dark mode apply

--- src/app/shared/components/skeleton-loader/skeleton-loader.component.ts
+++ src/app/shared/components/skeleton-loader/skeleton-loader.component.ts
@@ -14,17 +14,33 @@ import { Component, ElementRef, OnInit } from '@angular/core';
       height: var(--skeleton-height);
       margin: 0 auto 12px auto;
 
-      background: linear-gradient(110deg, #ececec 8%, #f5f5f5 18%, #ececec 33%);
+      background: linear-gradient(90deg, var(--angular-protoweather-surface-1) 8%, var(--angular-protoweather-surface-3) 18%, var(--angular-protoweather-surface-1) 33%);
       background-size: 200% 100%;
-      animation: 1.5s shine linear infinite;
       border-radius: 12px;
-    }
+      opacity: 0.7;
+      animation: shine 1.5s cubic-bezier(0, 0, 0, 0) infinite;
 
     @keyframes shine {
       to{
         background-position-x: -200%;
       }
     }
+
+    @keyframes pulse {
+    0%{
+        opacity: 1;
+    }
+    
+
+    50%{
+        opacity: 0.5;
+    }
+
+    100%{
+        opacity: 1;
+    }
+  }
+}
   `]
 })
 export class SkeletonLoaderComponent implements OnInit {
