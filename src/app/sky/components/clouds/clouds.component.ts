470ca77657852669d03a3a7cb94cce7480032b32
jhonnoriega1221 <jhonlex17@gmail.com>
2023-01-16 12:41:39 -0500

    feat: all clouds animated

--- src/app/sky/components/clouds/clouds.component.ts
+++ src/app/sky/components/clouds/clouds.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'pw-clouds',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class CloudsComponent {
 
+  @Input() cloudsLevel:number = 0;
 }
