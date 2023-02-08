c5ee6f45eef20646d16b33e0fd7588654ea69529
jhonnoriega1221 <jhonlex17@gmail.com>
2023-02-08 12:57:38 -0500

    feat: add app name to environment file

--- src/app/weather/weather-routing.module.ts
+++ src/app/weather/weather-routing.module.ts
@@ -5,8 +5,9 @@ import { SetlocationPageComponent } from './pages/setlocation-page/setlocation-p
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { FavoritesPageComponent } from './pages/favorites-page/favorites-page.component';
 import { ForecastPageComponent } from './pages/forecast-page/forecast-page.component';
+import { environment } from 'src/environments/environment';
 
-const nameApp:string = 'Protoweather';
+const nameApp:string = environment.appName;
 
 const routes: Routes = [
   { path: '', component: WeatherLayoutComponent,
