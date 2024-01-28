23c0a7f626863d9644bc1e8a9af1b37a19883f0a
jhonnoriega1221 <jhonlex17@gmail.com>
2024-01-27 21:53:23 -0500

    fix: remove angular universal and ssr

--- api/index.js
+++
@@ -1,3 +0,0 @@
-const server = require('../dist/angular-protoweather/server/main');
-
-module.exports = server.app();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { TransferHttpCacheModule } from '@nguniversal/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,11 +11,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule,
-    TransferHttpCacheModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
