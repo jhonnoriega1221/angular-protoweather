3824083caab50e0e1eb8ea690f5a89496b69dd0f
jhonnoriega1221 <jhonlex17@gmail.com>
2024-02-10 18:12:34 -0500

    feat: add fade in animation to clouds when get from source

--- src/app/sky/components/clouds/clouds.component.ts
+++ src/app/sky/components/clouds/clouds.component.ts
@@ -1,14 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { ImageService } from 'src/app/ui/image/services/image.service';
 
 @Component({
   selector: 'pw-clouds',
   templateUrl: './clouds.component.html',
   styleUrls: ['./clouds.component.scss']
 })
-export class CloudsComponent {
+export class CloudsComponent implements AfterViewInit {
 
   @Input() cloudsLevel:number = 5;
   @Input() actualHour:number = 0;
   @Input() isCloudy:boolean = false;
+  @ViewChildren('imageLoader') images!: QueryList<ElementRef<HTMLImageElement>>;
+
+  constructor(private imageService: ImageService){}
+
+  ngAfterViewInit(): void {
+      this.images.forEach( (image) => {
+        const cloudFileName = image.nativeElement.classList[1];
+        this.imageService.load(`/assets/weather_backgrounds/${cloudFileName}.webp`).subscribe( () => {
+          image.nativeElement.classList.add('loaded')
+        } )
+      } )
+  }
 
 }
